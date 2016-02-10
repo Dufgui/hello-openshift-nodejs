06c169cca366eb97296a8848e5b2e3c17fb82d9f
Duff <guillaume.duff@gmail.com>
2016-02-10 16:54:53 +0100

    again

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var port = process.env.PORT || 8080;
 
 http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello Duff from OpenShift\n");
+  response.end("Hello Duff 2 from OpenShift\n");
 }).listen(port);
 
 console.log("Server running on port " + port);
